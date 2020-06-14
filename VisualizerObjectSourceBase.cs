099e674fbd59e894db04eb36e2feecadbaec01df
Zev Spitz <shivisi@hotmail.com>
2020-06-14 08:46:13 +0300

    Create VisualizerObjectSourceBase

---
+++ VisualizerObjectSourceBase.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.DebuggerVisualizers;
+using System.IO;
+
+namespace Periscope.Debuggee {
+    public abstract class VisualizerObjectSourceBase<TTarget, TConfig> : VisualizerObjectSource  where TConfig : ConfigBase<TConfig> {
+        public override void GetData(object target, Stream outgoingData) => Serialize(outgoingData, "");
+
+        public override void TransferData(object target, Stream incomingData, Stream outgoingData) {
+            var config = (TConfig)Deserialize(incomingData);
+            var response = GenerateResponse((TTarget)target, config);
+            Serialize(outgoingData, response);
+        }
+
+        public abstract object GenerateResponse(TTarget target, TConfig config);
+    }
+}
