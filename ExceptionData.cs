795c8950b4dec6c09148ff923c2a55e41d190fd0
Zev Spitz <shivisi@hotmail.com>
2020-06-18 23:49:38 +0300

    Implement debuggee-side logging

---
+++ ExceptionData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Periscope.Debuggee {
+    [Serializable]
+    public class ExceptionData {
+        public ExceptionData(Exception ex, string? logMessage = null) {
+            TypeName = ex.GetType().Name;
+            Message = ex.Message;
+            StackTrace = ex.StackTrace;
+            if (ex.InnerException is { }) {
+                InnerException = new ExceptionData(ex.InnerException);
+            }
+            LogMessage = logMessage;
+        }
+
+        public string TypeName { get; }
+        public string Message { get; }
+        public string StackTrace { get; }
+        public ExceptionData? InnerException { get; }
+        public string? LogMessage { get; }
+
+        public override string ToString() =>
+            $@"Log message: {LogMessage}
+Typename: {TypeName}
+Message: {Message}
+InnerException: {InnerException?.TypeName}
+
+Stack trace:
+{StackTrace}";
+    }
+}
--- VisualizerObjectSourceBase.cs
+++ VisualizerObjectSourceBase.cs
@@ -1,15 +1,50 @@
 ﻿using Microsoft.VisualStudio.DebuggerVisualizers;
 using System;
+using System.Diagnostics;
 using System.IO;
 
 namespace Periscope.Debuggee {
     public abstract class VisualizerObjectSourceBase<TTarget, TConfig> : VisualizerObjectSource  where TConfig : ConfigBase<TConfig> {
         public override void GetData(object target, Stream outgoingData) => Serialize(outgoingData, GetConfigKey());
 
+        // This is all far more complicated than necessary, because I haven't been able to attach a debugger to this code
         public override void TransferData(object target, Stream incomingData, Stream outgoingData) {
-            var config = (TConfig)Deserialize(incomingData);
-            var model = GetResponse((TTarget)target, config);
-            Serialize(outgoingData, model);
+            void logException(string logMessage, Exception ex) => Serialize(outgoingData, new ExceptionData(ex, logMessage));
+
+            TConfig? config;
+            try {
+                config = (TConfig)Deserialize(incomingData);
+            } catch (Exception ex) {
+                logException("Deserialize incoming config", ex);
+                return;
+            }
+
+            TTarget t;
+            if (target is TTarget t1) {
+                t = t1;
+            } else {
+                string message =
+                    target is null ?
+                        "Target is null." :
+                        $"Target is of type {target.GetType()}; expected {typeof(TTarget)}.";
+                logException("Mismatched target type", new Exception(message));
+                return;
+            }
+
+            object? model;
+            try {
+                model = GetResponse(t, config!);
+            } catch (Exception ex) {
+                logException("Get response", ex);
+                return;
+            }
+
+            try {
+                Serialize(outgoingData, model);
+            } catch (Exception ex) {
+                logException("Serialize outgoing", ex);
+                return;
+            }
         }
 
         public virtual string GetConfigKey() => "";
